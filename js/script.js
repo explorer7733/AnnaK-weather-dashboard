b2c931345aa2c97091a5afb4dba20f89b071335e
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-05-03 15:01:45 -0700

    add index.html, css and js folders, APIKey

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*My API Key (name:MoonLight) f6c9d2164ce729074aa0a044b7c73a36*/
+/*we'll create an API call using the Current Data portion of the OpenWeather API and will serach by city name*/
+
+const weatherApiKey = "f6c9d2164ce729074aa0a044b7c73a36";
+
+/*we wll collect user input for the city name and in stire it in varibale let city =    ; */
+/*need to specify state & country variables in API call */
+let cityName = 
+let stateName = 
+let countryName =
